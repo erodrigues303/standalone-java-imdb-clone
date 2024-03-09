46ec6237b94b1372bbca1e10e03bf566c73c0852
eldibyorku <eldib@my.yorku.ca>
2024-03-09 13:57:38 -0500

    "fixed main"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,36 +12,36 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                new LoginUI();
-//            }
-//        });
-
-        List<String> newCommonWords = Arrays.asList(
-                "Woman", "Child", "Adventure", "Quest", "Secret", "Mystery", "Magic", "Witch",
-                "Wizard", "Dragon", "Forest", "Desert", "Island", "Castle", "Tower", "Moon",
-                "Sun", "Galaxy", "Alien", "Robot", "Future", "Past", "Legend", "Myth", "Hero",
-                "Villain", "Escape", "Chase", "Race", "Battle", "Mission", "Dreams", "Nightmare",
-                "Love", "Hate", "Fear", "Courage", "Friendship", "Betrayal", "Survival", "Death",
-                "Life", "Joy", "Sorrow", "Fate", "Destiny", "Time", "Memory", "Ghost", "Spirit"
-        );
-MovieService ms = new MovieService();
-        MovieManager mm = new MovieManager();
-        for (String word:newCommonWords){
-            try {
-                List<Movie> movies = mm.searchMovies(word);
-                for (Movie movie : movies) {
-                    ms.createMovie(movie);
-                    System.out.println(movie.getMovieId());
-
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LoginUI();
             }
-        }
+        });
+
+//        List<String> newCommonWords = Arrays.asList(
+//                "Woman", "Child", "Adventure", "Quest", "Secret", "Mystery", "Magic", "Witch",
+//                "Wizard", "Dragon", "Forest", "Desert", "Island", "Castle", "Tower", "Moon",
+//                "Sun", "Galaxy", "Alien", "Robot", "Future", "Past", "Legend", "Myth", "Hero",
+//                "Villain", "Escape", "Chase", "Race", "Battle", "Mission", "Dreams", "Nightmare",
+//                "Love", "Hate", "Fear", "Courage", "Friendship", "Betrayal", "Survival", "Death",
+//                "Life", "Joy", "Sorrow", "Fate", "Destiny", "Time", "Memory", "Ghost", "Spirit"
+//        );
+//MovieService ms = new MovieService();
+//        MovieManager mm = new MovieManager();
+//        for (String word:newCommonWords){
+//            try {
+//                List<Movie> movies = mm.searchMovies(word);
+//                for (Movie movie : movies) {
+//                    ms.createMovie(movie);
+//                    System.out.println(movie.getMovieId());
+//
+//                }
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            } catch (JSONException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
 //        DbFunctions.getConnection();
 //        MovieManager movieManager = new MovieManager();
